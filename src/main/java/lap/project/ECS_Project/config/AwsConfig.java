17920239d489138d9264f5e9c094d320ae7bd9bf
Julius Agbame <julius.agbame@amalitech.com>
2025-03-15 16:06:55 +0000

    Made some changes to the aws config file

--- src/main/java/lap/project/ECS_Project/config/AwsConfig.java
+++ src/main/java/lap/project/ECS_Project/config/AwsConfig.java
@@ -6,6 +6,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,8 +17,8 @@ public class AwsConfig {
 
     @Bean
     public AmazonS3 amazonS3(){
-        return AmazonS3ClientBuilder
-                .standard()
+        return AmazonS3Client
+                .builder()
                 .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
                 .withRegion(Regions.US_EAST_1)
                 .build();
